abc80af6cfc8df0fdbfe49547fd559a3751f3654
cn2027 <cnicolasepa@gmail.com>
2015-06-21 17:51:38 -0400

    Create server.R

---
+++ server.R
@@ -0,0 +1,39 @@
+pkgTest <- function(x)
+{
+        if (!require(x,character.only = TRUE))
+        {
+                install.packages(x,dep=TRUE)
+                if(!require(x,character.only = TRUE)) stop("Package not found")
+        }
+}
+
+pkgTest("shiny")
+
+# Load data processing file
+source("DATA_PROCESSING.R")
+#themes <- sort(unique(data$theme))
+
+# Shiny server
+shinyServer(
+        function(input, output) {
+                #     output$text1 <- renderText({input$text1})
+                #     output$text2 <- renderText({input$text2})
+                output$dTable <- renderDataTable({
+                        data
+                } #, options = list(bFilter = FALSE, iDisplayLength = 50)
+                )
+                
+                output$plotPopulationByYear <- renderChart({
+                        plotPopulationByYear(dt) 
+                })
+                
+                output$PopulationByEpoch <- renderChart({
+                        PopulationByEpoch(dt)
+                })
+                
+                output$piecesByThemeAvg <- renderChart({
+                        piecesByThemeAvg(dt)
+                })
+                
+        } # end of function(input, output)
+)
